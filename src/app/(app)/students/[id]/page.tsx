7b734396a6304ace56af2ec818bcd5e55376983d
provost <simonmkaro@gmail.com>
2025-12-29 13:20:38 +0200

    git commit -m "feat: add alert dialog footer to student page"

--- src/app/(app)/students/[id]/page.tsx
+++ src/app/(app)/students/[id]/page.tsx
@@ -52,6 +52,7 @@ import {
   AlertDialogCancel,
   AlertDialogContent,
   AlertDialogHeader,
+  AlertDialogFooter,
   AlertDialogTitle,
   AlertDialogTrigger,
   AlertDialogDescription as AlertDialogDescriptionComponent,
